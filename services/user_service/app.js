5b60379e25ef9d0aab6332e1f229a720a667df03
andytt <andytonthat08@gmail.com>
2020-02-11 21:12:38 -0500

    Added example of a proper handshake between two services

---
+++ services/user_service/app.js
@@ -0,0 +1,25 @@
+var express    = require('express');
+
+var app        = express();
+var bodyParser = require('body-parser');
+
+// configure app to use bodyParser()
+// this will let us get the data from a POST
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = 2000;
+
+var router = express.Router();
+
+router.route('/login')
+.get(function(req,res){
+    res.set('Access-Control-Allow-Origin', '*');
+    res.json({thisIsMyKey: 'ThisIsMyBody'});
+});
+
+app.use('/user', router);
+
+app.listen(port, () => {
+    console.log("Port 2000 working!");
+});
