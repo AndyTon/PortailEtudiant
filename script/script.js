f3e0d34df2c6b50ad66158e0af32c6dfd5516f11
Oualid Ben Amara <ben_amara.oualid@courrier.uqam.ca>
2020-03-10 12:03:19 -0400

    formulaire d inscription

---
+++ script/script.js
@@ -0,0 +1,180 @@
+const firstName = document.getElementById('firstName');
+const lastName = document.getElementById('lastName');
+const password = document.getElementById('password');
+const confirmPassword = document.getElementById('confirmPassword');
+const email = document.getElementById('email');
+
+
+// Form
+const form = document.getElementById('myForm');
+// colors
+const green = '#4CAF50';
+const red = '#F44336';
+
+//confirmation msg
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  if (
+    validateFirstName() &&
+    validateLastName() &&
+    validatePassword() &&
+    validateConfirmPassword() &&
+    validateEmail()
+  ) {
+    const name = firstName.value;
+    const container = document.querySelector('div.container');
+    const loader = document.createElement('div');
+    loader.className = 'progress';
+    const loadingBar = document.createElement('div');
+    loadingBar.className = 'indeterminate';
+    loader.appendChild(loadingBar);
+    container.appendChild(loader);
+    setTimeout(function() {
+      const loaderDiv = document.querySelector('div.progress');
+      const panel = document.createElement('div');
+      panel.className = 'card-panel green';
+      const text = document.createElement('span');
+      text.className = 'white-text';
+      text.appendChild(
+        document.createTextNode(
+          `Inscription réussie, bienvenue sur le protail étudiant ${name}`
+        )
+      );
+      panel.appendChild(text);
+      container.replaceChild(panel, loaderDiv);
+    }, 1000);
+  }
+});
+
+// Validators
+///////////////////////////////////////////
+
+function validateFirstName() {
+  if (checkIfEmpty(firstName)) return;
+  if (!checkIfOnlyLetters(firstName)) return;
+  return true;
+}
+function validateLastName() {
+  if (checkIfEmpty(lastName)) return;
+  if (!checkIfOnlyLetters(lastName)) return;
+  return true;
+}
+function validatePassword() {
+  if (checkIfEmpty(password)) return;
+  if (!meetLength(password, 4, 100)) return;
+  return true;
+}
+function validateConfirmPassword() {
+  if (password.className !== 'valid') {
+    setInvalid(confirmPassword, 'Le mot de passe doit être valide');
+    return;
+  }
+  if (password.value !== confirmPassword.value) {
+    setInvalid(confirmPassword, 'les mots de passe doivent correspondre');
+    return;
+  } else {
+    setValid(confirmPassword);
+  }
+  return true;
+}
+function validateEmail() {
+  if (checkIfEmpty(email)) return;
+  if (!containsCharacters(email, 5)) return;
+  return true;
+}
+function checkIfEmpty(field) {
+  if (isEmpty(field.value.trim())) {
+    setInvalid(field, `${field.name} ne doit pas être vide`);
+    return true;
+  } else {
+    setValid(field);
+    return false;
+  }
+}
+function isEmpty(value) {
+  if (value === '') return true;
+  return false;
+}
+function setInvalid(field, message) {
+  field.className = 'invalid';
+  field.nextElementSibling.innerHTML = message;
+  field.nextElementSibling.style.color = red;
+}
+function setValid(field) {
+  field.className = 'valid';
+  field.nextElementSibling.innerHTML = '';
+}
+function checkIfOnlyLetters(field) {
+  if (/^[a-zA-Z ]+$/.test(field.value)) {
+    setValid(field);
+    return true;
+  } else {
+    setInvalid(field, `${field.name} ne doit contenir que des lettres`);
+    return false;
+  }
+}
+function meetLength(field, minLength, maxLength) {
+  if (field.value.length >= minLength && field.value.length < maxLength) {
+    setValid(field);
+    return true;
+  } else if (field.value.length < minLength) {
+    setInvalid(
+      field,
+      `${field.name} doit être au moins ${minLength} caractères`
+    );
+    return false;
+  } else {
+    setInvalid(
+      field,
+      `${field.name} doit être plus court que ${maxLength} caractères`
+    );
+    return false;
+  }
+}
+function containsCharacters(field, code) {
+  let regEx;
+  switch (code) {
+    case 1:
+      regEx = /(?=.*[a-zA-Z])/;
+      return matchWithRegEx(regEx, field, 'Doit contenir au moins une lettre');
+    case 2:
+      regEx = /(?=.*\d)(?=.*[a-zA-Z])/;
+      return matchWithRegEx(
+        regEx,
+        field,
+        'Doit contenir au moins une lettre et un chiffre'
+      );
+    case 3:
+      regEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])/;
+      return matchWithRegEx(
+        regEx,
+        field,
+        'Doit contenir au moins une majuscule, une lettre minuscule et un chiffre'
+      );
+    case 4:
+      // uppercase, lowercase, number and special char
+      regEx = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*\W)/;
+      return matchWithRegEx(
+        regEx,
+        field,
+        'Doit contenir au moins une majuscule, une lettre minuscule, un chiffre et un caractère spécial'
+      );
+    case 5:
+      // Email pattern
+      regEx = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+      return matchWithRegEx(regEx, field, 'l adresse courriel doit être une adresse valide');
+    default:
+      return false;
+  }
+}
+function matchWithRegEx(regEx, field, message) {
+  if (field.value.match(regEx)) {
+    setValid(field);
+    return true;
+  } else {
+    setInvalid(field, message);
+    return false;
+  }
+}
+
+
